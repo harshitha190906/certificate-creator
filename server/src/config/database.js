6704df5b6158f77a6faa0d3673cddd689c544803
Harshitha Bantrothu <harshithabantrothu7@gmail.com>
2026-03-01 15:11:40 +0530

    Create database.js configuration file

---
+++ server/src/config/database.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    DATABASE_HOST: 'localhost',
+    DATABASE_PORT: 5432,
+    DATABASE_USER: 'your_username',
+    DATABASE_PASSWORD: 'your_password',
+    DATABASE_NAME: 'your_database',
+};
